4a0db0dc71a6feab663f39c79bb44d5e4a961246
Wenderson07 <Wenderson_pac@hotmail.com>
2016-11-07 14:52:19 -0200

    Create [incomp]q2.c

---
+++ Lista-6/[incomp]q2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+	int i,nota =0;
+	char gab[5];
+	printf("Digite o gabarito: ");
+	scanf("%s",gab);
+	for(i =1; i == 20;i++){
+	printf("Digite as resposta do aluno %d",i)
+		scanf("%d",)
+	
+return 0;	
+}
